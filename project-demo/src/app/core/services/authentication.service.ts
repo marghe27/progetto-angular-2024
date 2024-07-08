44e3b76578c850f1d709b80236ae6465795349cc
marghe27 <marghe27@live.it>
2024-07-08 23:28:58 +0200

    changed deleteUser() method

--- project-demo/src/app/core/services/authentication.service.ts
+++ project-demo/src/app/core/services/authentication.service.ts
@@ -163,21 +163,21 @@ export class AuthenticationService {
   }
 
   /** Nuovo metodo deleteUser()
-   * Utilizza l'User ID salvato nel localStorage
+   * Utilizza l'User aggiunto come parametro 
+   * modificato il 08/07/24 
    */
-  deleteUser(): Observable<void> {
-    if (!this.authentication|| !this.user) {
-      console.error('User not authenticated or user details are not available'); // Log di errore
+  deleteUser(user: User): Observable<void> {
+    if (!this.authentication) {
+      console.error('User not authenticated and not authorized for deletion'); // Log di errore
       throw new Error('User not authenticated or user details are not available');
     }
 
     const token = localStorage.getItem(ACCESS_TOKEN);
-    const userId = Number(localStorage.getItem('userId'));
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
     console.log('Delete User Headers:', headers); // Log di debug delle intestazioni
-    return this.http.delete<void>(`${this.apiUrl}/${this.authentication.id}`, { headers }).pipe(
+    return this.http.delete<void>(`${this.apiUrl}/${user.id}`, { headers }).pipe(
       tap(() => {
         this.logout().subscribe();
       }),
@@ -210,158 +210,4 @@ export class AuthenticationService {
 
 }
 
-/** Il codice seguente stava dentro al costruttore */
-    /* const storedToken = localStorage.getItem(ACCESS_TOKEN);
 
-    if(storedToken){
-      const storedAuthentication: Authentication = JSON.parse(storedToken);
-      const dateNow = new Date(); 
-      const expirationDate = new Date(storedAuthentication.expirationDate);
-      if (dateNow < expirationDate){
-        this.authentication = storedAuthentication;
-        this.isLoggedIn = true; 
-        // Get user data from local storage, If available
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-          this.user = JSON.parse(storedUser);
-        }
-      } else {
-        this.logout();
-      } 
-    } *//** Fine codice dentro al costruttore */
-
-  /** Login Method collegato al contenuto del constructor */
-  /* login(email:string, password:string): Observable<AuthenticationResponse>{
-   return this.http.post<AuthenticationResponse>(`${this.apiUrl}/login`, {email, password}).pipe(
-    tap(response => {
-      if(response.token){
-        const loginDate = new Date();
-        const expirationDate = new Date(loginDate.getTime() + (60 * 60000)); // 1 ora
-        this.authentication = { email, loginDate, expirationDate };
-        
-        localStorage.setItem(ACCESS_TOKEN, JSON.stringify(this.authentication));
-        //console.log('Token set:', localStorage.getItem(ACCESS_TOKEN));
-        this.isLoggedIn = true; 
-
-        // Let's get user details to obtain the ID 
-        this.getUserDetails(email).subscribe(user => {
-          this.user = user;
-          localStorage.setItem('user', JSON.stringify(this.user)); // Save user details into local storage
-        });
-      }
-    })); 
-  } */
-
-  /** Registration Method  (vecchio)
-  register(
-    name: string,
-    surname: string,
-    email: string,
-    password: string
-    ): Observable<AuthenticationResponse>{
-      return this.http.post<AuthenticationResponse>(`${this.apiUrl}/register`, {name, surname, email, password}).pipe(
-       tap(response => {
-        if (response.token) {
-          console.log('Registration successful');
-        } else {
-          console.log('Registration failed');
-        }
-       }));
-       
-     }*/
-
-  /* Vecchio metodo collegato alla variabile in alto commentata "private authentication?: Authentication;" /* Useful when other components or services need access to the user's authentication information, such as username or login/expiration dates.
- getAuthentication(): Authentication | undefined {
-    return this.authentication;
-   } */
-
-    /** Get User Details (vecchio)
-    getUserDetails(email: string): Observable<User> {
-      console.log(`Fetching user details for email: ${email}`); // Log per debug
-      return this.http.get<User[]>(`${this.apiUrl}?email=${email}`).pipe(
-        tap(users => console.log('Users retrieved:', users)), // Aggiungi un log per i dati restituiti
-        map(users =>{
-          this.user = users.find(u => u.email === email);  
-          if(!this.user){
-            throw new Error('User not found');
-          }
-          console.log(`User found: ${JSON.stringify(this.user)}`); // Log per debug
-          return this.user;
-        })
-      );
-    } */
-
-    /** Update User Details  (vecchio)
-    updateUserDetails(user: Partial<User>): Observable<User>{
-      if(!this.user || !this.user.id){
-        throw new Error('User not authenticated');
-      }
-      const token = localStorage.getItem(ACCESS_TOKEN);
-      if(!token){
-        throw new Error('Token not found in local storage');
-      }
-      const parsedToken = JSON.parse(token).token;
-      const headers = new HttpHeaders({
-      'Authorization': `Bearer ${parsedToken}`
-      });
-      return this.http.put<User>(`${this.apiUrl}/${user.id}`, user, { headers }).pipe(
-        tap(updateUser => {
-          this.user = {...this.user, ...updateUser};
-          // update the local storage
-          localStorage.setItem('user', JSON.stringify(this.user));
-        })
-      );
-    }*/
-
-  /** Delete User Account  (vecchio)
-  deleteUserAccount(): Observable<void> {
-    if (!this.user || !this.user.id) {
-      throw new Error('User not authenticated');
-    }
-
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-
-    return this.http.delete<void>(`${this.apiUrl}/${this.user.id}`, {headers}).pipe(
-      tap(() => {
-        this.logout().subscribe();
-      })
-    );
-  }*/
-
-  /** Logout Method   (vecchio) */
-  /* logout(): Observable<void> {
-    return new Observable<void>((observer) => {
-      
-      localStorage.removeItem(ACCESS_TOKEN);
-      localStorage.removeItem('user'); */
-      /* const tokenAfterRemoval = localStorage.getItem(ACCESS_TOKEN);
-      console.log('Token after removal:', tokenAfterRemoval);
-  
-      if (tokenAfterRemoval === null) {
-        console.log('Token successfully removed');
-      } else {
-        console.warn('Token was not removed, attempting to clear the local storage');
-        localStorage.clear();
-        console.log('Local storage cleared');
-      } */
-  
-      /* this.authentication = undefined;
-      this.user = undefined;
-      this.isLoggedIn = false;
-      observer.next();
-      observer.complete();
-    });
-  } */// fine /** Logout Method */
-  
-/* logout(): Observable<void> {
-      return new Observable<void>((observer) => {
-        localStorage.removeItem(ACCESS_TOKEN);
-        localStorage.removeItem('userId');
-        this.authentication = undefined;
-        observer.next();
-        observer.complete();
-      });
-    } */
